05c8a607840134108f174037ce15409b4d15cda1
Max <mchubar@mail.ru>
2024-08-24 14:09:53 +0600

    Update average.js

--- hw4/modules/average.js
+++ hw4/modules/average.js
@@ -2,6 +2,6 @@ export function calculateAverage() {
 	const a = prompt('Введите 1 число:')
 	const b = prompt('Введите 2 число:')
 	const c = prompt('Введите 3 число:')
-	let result = (parseFloat(a) + parseFloat(b) + parseFloat(c)) / 3
+	let result = Math.round((parseFloat(a) + parseFloat(b) + parseFloat(c)) / 3)
 	alert('Среднее значение 3 чисел: ' + result)
 }
