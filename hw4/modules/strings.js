be45ef373a5469eb2a22c5fa14968a7f3ac861a3
Max <mchubar@mail.ru>
2024-08-24 14:11:03 +0600

    Update strings.js

--- hw4/modules/strings.js
+++ hw4/modules/strings.js
@@ -1,6 +1,5 @@
 export function concatStrings() {
 	const word1 = prompt('Введите 1 слово:')
 	const word2 = prompt('Введите 2 слово:')
-	// let result = "Первое слово - «word1» , второе слово - «word2» "
 	alert('Первое слово - «' + word1 + '» , второе слово - «' + word2 + '» ')
 }
